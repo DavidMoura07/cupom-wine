dbbbfb8750085632be0f1039c60c680685c72887
David de Moura Marques <davidmarques@C0258.wine.com.br>
2019-11-28 16:13:39 -0300

    ajustes na imagem

--- src/app.service.ts
+++ src/app.service.ts
@@ -7,24 +7,24 @@ export class AppService {
   
     async editImage(cupom: string){
 
-        const START = 180;
+        let LEFT_START = 140 //180;
         const TOTAL_SPACE = 700;
         const SIZE_CHAR = 38; 
         const MIN_SIZE = 10;
-        let HIGH = 1460;
+        let HIGH = 1025;
 
         if(cupom.length < MIN_SIZE){
-            HIGH += 80;
+            LEFT_START -= 60;
         }
 
         cupom = cupom.toUpperCase();
 
-        const font = await Jimp.loadFont(Jimp.FONT_SANS_64_WHITE);
+        const font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE);
         const image = await Jimp.read('./img/cupom.png');
         return await image.print(
             font,
             // Centering string
-            START + ((TOTAL_SPACE - (cupom.length * SIZE_CHAR)) / 2),
+            LEFT_START + ((TOTAL_SPACE - (cupom.length * SIZE_CHAR)) / 2),
             HIGH,
             cupom,
             500,
